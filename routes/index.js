7197c2dcb0f33e93ae9e11b4fe3433b3ce698b9c
Rigoberto <rresendez@rigoresendez.com>
2016-12-22 01:24:24 -0600

    Working fluctiation system

--- routes/index.js
+++ routes/index.js
@@ -21,8 +21,22 @@ var client = new twilio.RestClient('ACa57bd2a6cc2a50aa56ed4b2bd1e0577b','4f5eb49
 //This is where i get the json file from
 
 var link="http://api.coindesk.com/v1/bpi/currentprice.json"
-var datg="";
+var datg,datx="";
+var bool=false;
+//One percent fluctuation function
 
+function fluc (v1,v2){
+  var onep = v1/100;
+  var point = onep/2;
+  if(v2>v1+point||v2<v1-point){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+//Get original value for bitcoin as datg
     getJSON(link, function (error,data) {
       if(error){
         console.log(error);
@@ -34,6 +48,73 @@ var datg="";
       datg=datg.toFixed(2);
       console.log(datg);
     })
+    //Check for fluctuation
+cron.schedule('*/10 * * * * *', function (){
+// Get current value of bitcoin
+
+  getJSON(link, function (error,data) {
+    if(error){
+      console.log(error);
+    }
+    var datc,datf;
+    datc = data.bpi;
+    datf = datc.USD;
+    datx = Number(datf.rate);
+    datx=datx.toFixed(2);
+
+  })
+  console.log(bool);
+  console.log("dat g value " + datg);
+  console.log("dat x value "+ datx);
+
+  var t1,t2;
+  t1=Math.floor(datg);
+  t2=Math.floor(datx);
+  console.log(t1,t2);
+  //Test for fluctuation
+  bool=fluc(t1,t2);
+  console.log(bool);
+
+  if(bool){
+    console.log("This is test for cron");
+
+
+    client.sms.messages.create({
+        to:'9564292948',
+        from:'9563771377',
+        body:'There was a 1% bitcoin fluctuation from: '+datg + ' to ' +datx,
+    }, function(error, message) {
+        // The HTTP request to Twilio will run asynchronously. This callback
+        // function will be called when a response is received from Twilio
+        // The "error" variable will contain error information, if any.
+        // If the request was successful, this value will be "falsy"
+        if (!error) {
+            // The second argument to the callback will contain the information
+            // sent back by Twilio for the request. In this case, it is the
+            // information about the text messsage you just sent:
+            console.log('Success! The SID for this SMS message is:');
+            console.log(message.sid);
+
+            console.log('Message sent on:');
+            console.log(message.dateCreated);
+            //Update value of datg
+            datg=datx;
+        } else {
+            console.log('Oops! There was an error.');
+            console.log(error);
+        }
+    });
+
+  }
+  else{
+    console.log("No fluctuation");
+    console.log("");
+    console.log("/*******************NEW ITERATION**********************/")
+
+  }
+
+
+})
 
 
 
@@ -42,34 +123,9 @@ var datg="";
 
 
 /* GET home page. */
-cron.schedule('*/10 * * * * *', function(){
-  console.log("This is test for cron");
 
-  client.sms.messages.create({
-      to:'9562223307',
-      from:'9563771377',
-      body:'Current price of bitcoin '+datg
-  }, function(error, message) {
-      // The HTTP request to Twilio will run asynchronously. This callback
-      // function will be called when a response is received from Twilio
-      // The "error" variable will contain error information, if any.
-      // If the request was successful, this value will be "falsy"
-      if (!error) {
-          // The second argument to the callback will contain the information
-          // sent back by Twilio for the request. In this case, it is the
-          // information about the text messsage you just sent:
-          console.log('Success! The SID for this SMS message is:');
-          console.log(message.sid);
 
-          console.log('Message sent on:');
-          console.log(message.dateCreated);
-      } else {
-          console.log('Oops! There was an error.');
-          console.log(error);
-      }
-  });
 
-})
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
